a6be42e6551b108d1e3e35b31bc7399a3c744314
MatthGitHub <matiasbenditti@gmail.com>
2016-11-08 21:53:18 -0300

    Agrego menu principal con sectores y mesas a planificar como va ser, proveedores view y proveedores nuevo

--- app/src/gui/main/Main.java
+++ app/src/gui/main/Main.java
@@ -5,19 +5,84 @@
  */
 package gui.main;
 
+import gui.login.LoginView;
+import gui.menuPrincipal.MenuPrincipal;
+import gui.proveedores.ProveedoresView;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Matth
  */
-public class Main extends javax.swing.JFrame {
-
+public final class Main extends javax.swing.JFrame {
+/* -------------------  Variables views  ------------------------------------------- */
+    private LoginView loginView;
+    private MenuPrincipal menuPpalView;
+    private ProveedoresView proveedoresView;
+/* -------------------  Variables views  ------------------------------------------- */    
     /**
      * Creates new form Main
      */
     public Main() {
         initComponents();
+        setDefaultCloseOperation(0);
+        setSize(420, 310);
+        jmb_menu.setVisible(false);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setTitle("Maraja");
+        goLoginView();
     }
-
+    
+    /**
+     * Ingreso al login
+     */
+    public void goLoginView(){
+        loginView = LoginView.getLoginView(this);
+        if(!loginView.isVisible() == false){
+            getContentPane().add(loginView);
+        }else{
+            loginView.setVisible(true);
+        }
+    }
+    
+    /**
+     * Ingreso al menu principal
+     */
+    public void goMenuPrincipal(){
+        menuPpalView = MenuPrincipal.getMenuPrincipal(this);
+        if(!menuPpalView.isVisible() == false){
+            getContentPane().add(menuPpalView);
+            setSize(800, 600);
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+            jmb_menu.setVisible(true);
+            revalidate();
+        }else{
+            menuPpalView.setVisible(true);
+            setSize(800, 600);
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+            jmb_menu.setVisible(true);
+            revalidate();
+        }
+    }
+    
+    /**
+     * Ingreso a la vista de todos los proveedores
+     */
+    public void goProveedoresView(){
+        proveedoresView = ProveedoresView.getProveedoresView(this);
+        if(!proveedoresView.isVisible() == false){
+            getContentPane().add(proveedoresView);
+        }else{
+            proveedoresView.setVisible(true);
+        }
+        revalidate();
+    }
+    
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,21 +92,189 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jmb_menu = new javax.swing.JMenuBar();
+        jm_archivo = new javax.swing.JMenu();
+        mi_menuPrincipal = new javax.swing.JMenuItem();
+        mi_salir = new javax.swing.JMenuItem();
+        jm_administracion = new javax.swing.JMenu();
+        jm_estadisticas = new javax.swing.JMenu();
+        mi_ventas = new javax.swing.JMenuItem();
+        mi_compras = new javax.swing.JMenuItem();
+        jm_configuracion = new javax.swing.JMenu();
+        jm_productos = new javax.swing.JMenu();
+        mi_nuevoProducto = new javax.swing.JMenuItem();
+        mi_listarProductos = new javax.swing.JMenuItem();
+        jm_proveedores = new javax.swing.JMenu();
+        mi_nuevoProveedor = new javax.swing.JMenuItem();
+        mi_listarProveedores = new javax.swing.JMenuItem();
+        jm_mesas = new javax.swing.JMenu();
+        mi_nuevaMesa = new javax.swing.JMenuItem();
+        mi_listarMesas = new javax.swing.JMenuItem();
+        jm_usuarios = new javax.swing.JMenu();
+        mi_nuevoUsuario = new javax.swing.JMenuItem();
+        mi_listarUsuarios = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 800, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
-        );
+        jm_archivo.setText("Archivo");
+
+        mi_menuPrincipal.setText("Menu Principal");
+        mi_menuPrincipal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_menuPrincipalActionPerformed(evt);
+            }
+        });
+        jm_archivo.add(mi_menuPrincipal);
+
+        mi_salir.setText("Salir");
+        mi_salir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_salirActionPerformed(evt);
+            }
+        });
+        jm_archivo.add(mi_salir);
+
+        jmb_menu.add(jm_archivo);
+
+        jm_administracion.setText("Administracion");
+
+        jm_estadisticas.setText("Estadisticas");
+
+        mi_ventas.setText("Ventas");
+        jm_estadisticas.add(mi_ventas);
+
+        mi_compras.setText("Compras");
+        mi_compras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_comprasActionPerformed(evt);
+            }
+        });
+        jm_estadisticas.add(mi_compras);
+
+        jm_administracion.add(jm_estadisticas);
+
+        jmb_menu.add(jm_administracion);
+
+        jm_configuracion.setText("Configuracion");
+
+        jm_productos.setText("Productos");
+
+        mi_nuevoProducto.setText("Nuevo");
+        mi_nuevoProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_nuevoProductoActionPerformed(evt);
+            }
+        });
+        jm_productos.add(mi_nuevoProducto);
+
+        mi_listarProductos.setText("Ver todos");
+        mi_listarProductos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_listarProductosActionPerformed(evt);
+            }
+        });
+        jm_productos.add(mi_listarProductos);
+
+        jm_configuracion.add(jm_productos);
+
+        jm_proveedores.setText("Proveedores");
+
+        mi_nuevoProveedor.setText("Nuevo");
+        mi_nuevoProveedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_nuevoProveedorActionPerformed(evt);
+            }
+        });
+        jm_proveedores.add(mi_nuevoProveedor);
+
+        mi_listarProveedores.setText("Ver todos");
+        mi_listarProveedores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_listarProveedoresActionPerformed(evt);
+            }
+        });
+        jm_proveedores.add(mi_listarProveedores);
+
+        jm_configuracion.add(jm_proveedores);
+
+        jm_mesas.setText("Mesas");
+
+        mi_nuevaMesa.setText("Nueva");
+        mi_nuevaMesa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_nuevaMesaActionPerformed(evt);
+            }
+        });
+        jm_mesas.add(mi_nuevaMesa);
+
+        mi_listarMesas.setText("Ver todas");
+        jm_mesas.add(mi_listarMesas);
+
+        jm_configuracion.add(jm_mesas);
+
+        jm_usuarios.setText("Usuarios");
+
+        mi_nuevoUsuario.setText("Nuevo");
+        mi_nuevoUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mi_nuevoUsuarioActionPerformed(evt);
+            }
+        });
+        jm_usuarios.add(mi_nuevoUsuario);
+
+        mi_listarUsuarios.setText("Ver todos");
+        jm_usuarios.add(mi_listarUsuarios);
+
+        jm_configuracion.add(jm_usuarios);
+
+        jmb_menu.add(jm_configuracion);
+
+        setJMenuBar(jmb_menu);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void mi_listarProductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_listarProductosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_listarProductosActionPerformed
+
+    private void mi_nuevoProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_nuevoProductoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_nuevoProductoActionPerformed
+
+    private void mi_nuevoProveedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_nuevoProveedorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_nuevoProveedorActionPerformed
+
+    private void mi_nuevaMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_nuevaMesaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_nuevaMesaActionPerformed
+
+    private void mi_nuevoUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_nuevoUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_nuevoUsuarioActionPerformed
+
+    private void mi_comprasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_comprasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_comprasActionPerformed
+
+    private void mi_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_salirActionPerformed
+        // TODO add your handling code here:
+        if (JOptionPane.showConfirmDialog(this, "Seguro desea salir?", "Confirmar", JOptionPane.YES_NO_OPTION) == 0) {
+            System.exit(0);
+        }
+    }//GEN-LAST:event_mi_salirActionPerformed
+
+    private void mi_listarProveedoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_listarProveedoresActionPerformed
+        // TODO add your handling code here:
+        this.getContentPane().removeAll();
+        goProveedoresView();
+        this.repaint();
+    }//GEN-LAST:event_mi_listarProveedoresActionPerformed
+
+    private void mi_menuPrincipalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mi_menuPrincipalActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mi_menuPrincipalActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +312,26 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jm_administracion;
+    private javax.swing.JMenu jm_archivo;
+    private javax.swing.JMenu jm_configuracion;
+    private javax.swing.JMenu jm_estadisticas;
+    private javax.swing.JMenu jm_mesas;
+    private javax.swing.JMenu jm_productos;
+    private javax.swing.JMenu jm_proveedores;
+    private javax.swing.JMenu jm_usuarios;
+    private javax.swing.JMenuBar jmb_menu;
+    private javax.swing.JMenuItem mi_compras;
+    private javax.swing.JMenuItem mi_listarMesas;
+    private javax.swing.JMenuItem mi_listarProductos;
+    private javax.swing.JMenuItem mi_listarProveedores;
+    private javax.swing.JMenuItem mi_listarUsuarios;
+    private javax.swing.JMenuItem mi_menuPrincipal;
+    private javax.swing.JMenuItem mi_nuevaMesa;
+    private javax.swing.JMenuItem mi_nuevoProducto;
+    private javax.swing.JMenuItem mi_nuevoProveedor;
+    private javax.swing.JMenuItem mi_nuevoUsuario;
+    private javax.swing.JMenuItem mi_salir;
+    private javax.swing.JMenuItem mi_ventas;
     // End of variables declaration//GEN-END:variables
 }
